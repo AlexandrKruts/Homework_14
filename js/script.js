6b62a7fff4f26db73c20b917c4ddeedf0e4a8bd1
AlexandrKruts <kruts704@gmail.com>
2022-09-30 18:19:14 +0300

    Done first task

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+//1
+// let user = localStorage.getItem("userData");
+// if (user !== null) {
+//   console.log(user);
+// } else {
+//   fetch("https://jsonplaceholder.typicode.com/users/1")
+//     .then((response) => response.json())
+//     .then((data) => {
+//       localStorage.setItem("userData", JSON.stringify(data));
+
+//     })
+//     .catch((error) => console.error(error));
+// }
