73faa555c3f3438aa47d94287700414fb55a5e5d
AlexandrKruts <kruts704@gmail.com>
2022-09-30 18:19:42 +0300

    Done second task

--- js/script.js
+++ js/script.js
@@ -13,3 +13,14 @@
 //     })
 //     .catch((error) => console.error(error));
 // }
+
+//2
+// function isValidDateFormat(str){
+// var regExp=/^\d{4}\/\d{2}\/\d{2}\s\d{2}:\d{2}$/;
+
+// return regExp.test(str)
+// }
+
+// console.log(isValidDateFormat("2012/09/18 12:10")); // true
+// console.log(isValidDateFormat("5012/88/55 99:88")); // true
+// console.log(isValidDateFormat("12.02.2022 12:10")); // false
